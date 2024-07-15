8b3f15452f5d89b56d27c0cacb63d75db0dfdbb3
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-07-16 02:40:11 +0600

    add: page initial loading

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Banner from "../components/Banner";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <img src="/src/assets/logo.png" alt="" />
+      </div>
+    );
+  }
   return (
     <div>
       <Banner />
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import Main from "../layouts/Main";
 import Dashboard from "../pages/Dashboard/Dashboard/Dashboard";
 
